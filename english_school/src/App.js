8808b9ecf484a1bc2a679a73242c4ff0662a6bf7
Alexander Dudnik <dudnik.code@gmail.com>
2022-10-18 18:23:59 +0300

    Commit 1

---
+++ english_school/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ english_school/src/Components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { FormControl, Nav, Navbar, Container, Button, Form } from "react-bootstrap";
+import logo from "./img/logo.png"
+
+function Header() {
+    return(
+        <>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img 
+                        src={logo} 
+                        height="37"
+                        width="157"
+                        className="d-inline-block align-top"
+                        alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content space-around" >
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/courses">Courses</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                            <Nav.Link href="/about">About us</Nav.Link>
+                            <Nav.Link href="/contacts">Contacts</Nav.Link>
+                        </Nav>
+                        <Form inline>
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header

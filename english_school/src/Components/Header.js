045ffb1230de239d4c2b6a6f9dad64fa8c11aade
Alexander Dudnik <dudnik.code@gmail.com>
2022-10-18 21:38:49 +0300

    Commit 3

--- english_school/src/Components/Header.js
+++ english_school/src/Components/Header.js
@@ -1,42 +1,59 @@
-import React from "react";
+import React, {Component} from "react";
 import { FormControl, Nav, Navbar, Container, Button, Form } from "react-bootstrap";
 import logo from "./img/logo.png"
 import"./Header.css"
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+
+import Home from '../Pages/Home'
+import Courses from '../Pages/Courses'
+import Blog from '../Pages/Blog'
+import About from '../Pages/About'
+import Contacts from '../Pages/Contacts'
 
 function Header() {
     return(
         <>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img 
-                        src={logo} 
-                        height="37"
-                        width="157"
-                        className="d-inline-block align-top"
-                        alt="Logo"
-                        />
-                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav" className="navbar_menu">
+                        <Navbar.Brand href="/">
+                            <img 
+                            src={logo} 
+                            height="37"
+                            width="157"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                            />
+                        </Navbar.Brand>
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/courses">Courses</Nav.Link>
                             <Nav.Link href="/blog">Blog</Nav.Link>
-                            <Nav.Link href="/about">About us</Nav.Link>
+                            <Nav.Link href="/About">About us</Nav.Link>
                             <Nav.Link href="/contacts">Contacts</Nav.Link>
                         </Nav>
-                        <Form inline className="header_form">
+                        <Form className="header_form">
                             <FormControl
                                 type="text"
                                 placeholder="Search"
                                 className="mr-sm-2"
                             />
-                            <Button variant="outline-info">Search</Button>
+                            <Button variant="warning">Search</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/courses" element={<Courses/>}/>
+                    <Route exact path="/blog" element={<Blog/>}/>
+                    <Route exact path="/About" element={<About/>}/>
+                    <Route exact path="/contacts" element={<Contacts/>}/>
+                </Routes>
+            </Router>
         </>
     )
 }
---
+++ english_school/src/Pages/Courses.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <div>
+      <h1>Courses</h1>
+    </div>
+  );
+}
+export default Courses
